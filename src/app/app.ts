fe9dc1f619054e37c4f7ec7994fb0182aa67eacd
ayazpathan <pathan.ayaz@outlook.com>
2026-07-16 22:07:50 +0530

    Created apollo server setup in app file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,27 @@
 import Fastify from "fastify";
+import fastifyApollo from "@as-integrations/fastify";
 
-export const buildApp = () => {
-    return Fastify({
+import { apolloServer } from "../core/apollo/server.js";
+import { buildContext } from "../core/apollo/context.js";
+
+export const buildApp = async () => {
+    const app = Fastify({
         logger: {
             level: "info",
         },
     });
+
+    await apolloServer.start();
+
+    await app.register(
+        fastifyApollo(apolloServer),
+        {
+            path: "/graphql",
+            context: async (request, reply) => {
+                return buildContext(request);
+            },
+        },
+    );
+
+    return app;
 };
